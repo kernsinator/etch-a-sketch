6052c5c15d731e77e84cb32aa5c6a6b06cbb35ac
Tyler Kerns <tkerns@protonmail.com>
2022-09-30 22:44:36 -0500

    make grid adapt to user input

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
 const container = document.querySelector('.container')
 
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement('div');
-    div.classList.add('cell')
-    container.appendChild(div);
+const defaultGrid = 16;
+let userChoice = 0;
+
+function handleClick(e) {
+    userChoice  = getUserInput();
+    drawGrid(userChoice);
+    userChoice = 0;
 }
 
-function paintCells(event) {
+function getUserInput() {
+    while (!(typeof(userChoice) === "number" && (userChoice > 0 && userChoice <= 100))) {
+        userChoice = +prompt('Enter number of squares per side for new grid: ')
+        return userChoice;
+    }
+    
+}
+
+const button = document.querySelector('.user-input')
+button.addEventListener('click', handleClick)
+
+function drawGrid(lengthOfSide) {
+    container.replaceChildren()
+    for (let i = 0; i < lengthOfSide; i++) {
+        for (let j = 0; j < lengthOfSide; j++) {
+            let div = document.createElement('div');
+            div.classList.add('cell');
+            div.style.width = `${800 / lengthOfSide}px`;
+            div.style.height = `${800 / lengthOfSide}px`;
+            div.addEventListener('mouseover', paintCell)
+            container.appendChild(div);
+        }
+    }
+}
+
+function paintCell(event) {
+    console.log(event);
     event.target.classList.add('painted')
 }
 
-const cells = document.querySelectorAll('.cell')
-cells.forEach(cell => cell.addEventListener('mouseover', paintCells));
+
+drawGrid(defaultGrid)
