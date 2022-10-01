8a9bd2446840ef9f8056f682504b94d03c5396c8
Tyler Kerns <tkerns@protonmail.com>
2022-09-30 19:16:41 -0500

    add hover effect

--- main.js
+++ main.js
@@ -2,5 +2,13 @@ const container = document.querySelector('.container')
 
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
+    div.classList.add('cell')
     container.appendChild(div);
 }
+
+function paintCells(event) {
+    event.target.classList.add('painted')
+}
+
+const cells = document.querySelectorAll('.cell')
+cells.forEach(cell => cell.addEventListener('mouseover', paintCells));
